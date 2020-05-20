3a0cd170a38f1d02d9af82d596e825503ed350ed
Ihsen SELLAOUTI <ihsen.sellaouti@estiam.com>
2020-05-20 14:45:27 +0200

    Load File Html with external css

--- samples/NodeApps/server_fs_async.js
+++ samples/NodeApps/server_fs_async.js
@@ -2,22 +2,43 @@ var http = require("http");
 var fs = require("fs");
 
 http.createServer((req, res) => {
-    fs.readFile('index.html', 'utf8', (err, data) => {    
-        if(err)
-        {
-            console.log(err);
-            res.writeHead(404, {'Content-Type' : 'text/html'});
-            res.write('<h1>Fichier introuvable.</h1>');
-        }
-        else
-        {
-            console.log(data);
-            res.writeHead(200, {'Content-Type' : 'text/html'});
-            res.write(data);
-        }
+    if(req.url.indexOf('.html') != -1){
+        fs.readFile('index.html', 'utf8', (err, data) => {    
+            if(err)
+            {
+                console.log(err);
+                res.writeHead(404, {'Content-Type' : 'text/html'});
+                res.write('<h1>Fichier introuvable.</h1>');
+            }
+            else
+            {
+                res.writeHead(200, {'Content-Type' : 'text/html'});
+                res.write(data);
+            }
+    
+            res.end();
+        });
+    }
 
-        res.end();
-    });
+    if(req.url.indexOf('.css') != -1)
+    {
+        fs.readFile(__dirname + '/css/style.css', (err, data) => {
+            if(err)
+            {
+                console.log(err);
+                res.writeHead(404, {'Content-Type' : 'text/html'});
+                res.write('<h1>Fichier introuvable.</h1>');
+            }
+            else
+            {
+                res.writeHead(200, {'Content-Type' : 'text/css'});
+                res.write(data);
+            }
+    
+            res.end();
+        });
+    }
+    
 }).listen(4000);
 
 console.log("Serveur lancé");
