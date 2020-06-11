7ba946adabf464c48f59679bffc8b6e8b6f57285
Ihsen SELLAOUTI <ihsen.sellaouti@estiam.com>
2020-06-11 11:33:38 +0200

    Gestion du template Pug

---
+++ samples/ExpressApps/template/00-global/template.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+const PORT = 3000;
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {title:"Utilisation du template Pug", message : "Génération de la page html"});
+});
+
+
+
+app.listen(PORT);
